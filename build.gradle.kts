78b24ffd5f1d55caa729450b1ca321b72ff6c839
Moin Uddin <eee.moin@gmail.com>
2024-04-21 22:51:20 +0200

    add test

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,9 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+	testImplementation("io.mockk:mockk:1.10.4")
+	testImplementation("com.ninja-squad:springmockk:3.0.1")
 
 	//logging
 	implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
@@ -43,4 +46,13 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+sourceSets{
+	test{
+		java {
+			setSrcDirs(listOf("src/test/intg", "src/test/unit"))
+		}
+
+	}
 }
